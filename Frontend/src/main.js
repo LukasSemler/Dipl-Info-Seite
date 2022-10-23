dd1a18f7f82094ce1285787a502bfb0e33bfb59d
LukasSemler <semler.l04@htlwienwest.at>
2022-10-23 19:36:53 +0200

    Neuer Recaptcha API Key

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -19,6 +19,6 @@ app.use(router);
 const pinia = createPinia();
 app.use(pinia);
 
-app.use(VueReCaptcha, { siteKey: '6Le3B2YiAAAAAOQlxu_PFxXN5K7rpb0wnzy9ZqsO' });
+app.use(VueReCaptcha, { siteKey: '6LfP5aUiAAAAAN9VE5x---r3Tgt2AtZsyhCJyZnN' });
 
 app.mount('#app');
